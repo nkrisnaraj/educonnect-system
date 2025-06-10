cb0945d1c9701b57b4798cbe6e2347ebeeccb7d9
Nihashini <nihashinisuba@gmail.com>
2025-06-10 12:33:40 +0530

    Added student module

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  darkMode: 'class', // <--- Add this
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2064d4",       
+        accent: "#568be3", 
+         
+      },
+    },
+  },
+  plugins: [],
+}
