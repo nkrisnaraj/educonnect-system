8a585827437096c8af350273c3ae09aa3a1943d5
Nihashini <nihashinisuba@gmail.com>
2025-06-12 11:51:16 +0530

    create student folder

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-red-500">
+        Home Page
+      </h1>
+    </div>
+  );
+}
