c38bbcdc9407fd477184156d5829c608f373a93b
Krisnaraj Navaraththinasingam <139768594+nkrisnaraj@users.noreply.github.com>
2025-06-13 11:58:26 +0530

    Admin : updated user registration , login serializer (#23)
    * updated gitignore file
    
    * updated gitignore file
    
    * Dev (#11)
    
    * studentapp added (#5)
    
    * modified
    
    * created edu_admin app
    
    * created edu_admin app (#9)
    
    * created edu_admin app
    
    * updated gitignore file
    
    * updated gitignore file
    
    * Create Students folder (#7)
    
    * studentapp added
    
    * Update dependencies
    
    * create student folder
    
    * created edu_admin app
    
    ---------
    
    Co-authored-by: nkrisnaraj <nkrisnaraj007@gmail.com>
    
    * updated
    
    ---------
    
    Co-authored-by: Nihashini Subatharan <nihashinisuba@gmail.com>
    
    * Dev (#12)
    
    * studentapp added (#5)
    
    * modified
    
    * created edu_admin app
    
    * created edu_admin app (#9)
    
    * created edu_admin app
    
    * updated gitignore file
    
    * updated gitignore file
    
    * Create Students folder (#7)
    
    * studentapp added
    
    * Update dependencies
    
    * create student folder
    
    * created edu_admin app
    
    ---------
    
    Co-authored-by: nkrisnaraj <nkrisnaraj007@gmail.com>
    
    * updated
    
    ---------
    
    Co-authored-by: Nihashini Subatharan <nihashinisuba@gmail.com>
    
    * Dev updated (#13)
    
    * studentapp added (#5)
    
    * modified
    
    * created edu_admin app
    
    * created edu_admin app (#9)
    
    * created edu_admin app
    
    * updated gitignore file
    
    * updated gitignore file
    
    * Create Students folder (#7)
    
    * studentapp added
    
    * Update dependencies
    
    * create student folder
    
    * created edu_admin app
    
    ---------
    
    Co-authored-by: nkrisnaraj <nkrisnaraj007@gmail.com>
    
    * updated
    
    ---------
    
    Co-authored-by: Nihashini Subatharan <nihashinisuba@gmail.com>
    
    * a
    
    * Delete backend/backend/__pycache__ directory
    
    * Delete backend/edu_admin/__pycache__ directory
    
    * Delete backend/edu_admin/migrations directory
    
    * Delete backend/students/__pycache__ directory
    
    * Delete backend/students/migrations directory
    
    * updated user registration and login serializer
    
    ---------
    
    Co-authored-by: Nihashini Subatharan <nihashinisuba@gmail.com>

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from .serializers import UserSerializer, RegisterSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+@api_view(['POST'])
+def register_user(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(request, username=username, password=password)
+
+    if user:
+        return Response(UserSerializer(user).data)
+    return Response({'error': 'Invalid username or password'}, status=status.HTTP_401_UNAUTHORIZED)
