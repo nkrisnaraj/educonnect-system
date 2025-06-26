ae82dc7dad94fdfdd9294aad1df96389201b6cf3
AbarnaKumarasamy1122 <Abarnasamy1122@gmail.com>
2025-06-26 17:45:54 +0530

    Remove .pyc and __pycache__ files from tracking

--- backend/students/urls.py
+++ backend/students/urls.py
@@ -1,9 +1,4 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import OnlinePaymentView, PayHereNotifyView
-from .views import OnlinePaymentView, PayHereNotifyView
-# from .views import OnlinePaymentView, ReceiptUploadView, PayHereNotifyView
-=======
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import OnlinePaymentView, ReceiptUploadView, PayHereNotifyView
@@ -12,22 +7,16 @@ from .views import CreatePayHereCheckoutUrl
 
 
     
->>>>>>> a8a3ecf80f4383e3046071a59c3ac0906236bf53
 
 
 
 urlpatterns = [
     path('payments/online/', OnlinePaymentView.as_view(), name='online-payment'),
     path('payhere-notify/', PayHereNotifyView.as_view(), name='payhere-notify'),
-<<<<<<< HEAD
-    # path("payments/upload-receipt",ReceiptUploadView.as_view(),name="upload-receipt"),
-    # path("payments/upload-receipt",ReceiptUploadView.as_view(),name="upload-receipt"),
-=======
     path("payments/upload-receipt/",ReceiptUploadView.as_view(),name="upload-receipt"),
     path("payment-info/",PaymentInfoView.as_view(),name="payment-info"),
     path("student-profile/",StudentProfileView.as_view(),name="student-profile"),
     path("payments/create-payhere-url/", CreatePayHereCheckoutUrl.as_view()),
->>>>>>> a8a3ecf80f4383e3046071a59c3ac0906236bf53
     
 ]
 
